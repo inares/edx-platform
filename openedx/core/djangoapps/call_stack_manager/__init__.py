fdfb21c5ec0fb2c2a9610fb726c5b36a8dcfe53a
utkjad <utkarsh@edx.org>
2015-06-15 14:41:38 +0000

    runing with models in tests/__init__.py VERSION2

--- openedx/core/djangoapps/call_stack_manager/__init__.py
+++ openedx/core/djangoapps/call_stack_manager/__init__.py
@@ -3,3 +3,4 @@ Root Package for getting call stacks of various Model classes being used
 """
 from __future__ import absolute_import
 from .core import CallStackManager, CallStackMixin, donottrack
+from .tests import ModelNothing, ModelWithCSMChild, ModelWithCSM, ModelMixin, ModelMixinCSM, ModelAnotherCSM
